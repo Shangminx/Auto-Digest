d9d5552fc72c2879f2d191b8830d48fb7796bbad
Shangminx <119831188+Shangminx@users.noreply.github.com>
2023-03-01 20:32:33 +0800

    Update a.py

--- a.py
+++ a.py
@@ -3,43 +3,52 @@ from datetime import datetime
 from datetime import timedelta
 import json
 
-headers = {
-    "Accept": "application/vnd.github+json",
-}
-baseurl = "https://api.github.com/repos/MicrosoftEdge/WebView2Feedback/issues"
+tries = 3
+for retrytimes in range(tries):
+    try:
+        headers = {
+            "Accept": "application/vnd.github+json",
+        }
+        baseurl = "https://api.github.com/repos/MicrosoftEdge/WebView2Feedback/issues"
 
-utcdate = (datetime.utcnow() - timedelta(days = 1)).date()
-issueList = []
-scanover = False
-page = 1
+        utcdate = (datetime.utcnow() - timedelta(days = 1)).date()
+        issueList = []
+        scanover = False
+        page = 1
 
-while(not scanover):
-    url = baseurl + '?page=' + str(page)
-    response = requests.get(url, headers=headers)
-    issues = response.json()
-    for item in issues:
-        date_object = datetime.strptime(item['created_at'], '%Y-%m-%dT%H:%M:%SZ').date()
+        while(not scanover):
+            url = baseurl + '?page=' + str(page)
+            response = requests.get(url, headers=headers)
+            issues = response.json()
+            for item in issues:
+                date_object = datetime.strptime(item['created_at'], '%Y-%m-%dT%H:%M:%SZ').date()
 
-        if(date_object < utcdate):
-            scanover = True
-            break
-        
-        newissue = {}
-        newissue['issueName'] = item['title']
-        newissue['issueLink'] = item['html_url']
-        tags = []
-        for label in item['labels']:
-            tags.append(label['name'])
-        newissue['issueTags'] = tags
-        if(item['assignee'] is not None):
-            newissue['issueAssignee'] = item['assignee']['login']
-        else:newissue['issueAssignee'] = None
+                if(date_object < utcdate):
+                    scanover = True
+                    break
 
-        if(date_object == utcdate):
-            issueList.append(newissue)
+                newissue = {}
+                newissue['issueName'] = item['title']
+                newissue['issueLink'] = item['html_url']
+                tags = []
+                for label in item['labels']:
+                    tags.append(label['name'])
+                newissue['issueTags'] = tags
+                if(item['assignee'] is not None):
+                    newissue['issueAssignee'] = item['assignee']['login']
+                else:newissue['issueAssignee'] = None
 
-    page = page + 1
+                if(date_object == utcdate):
+                    issueList.append(newissue)
 
-result = {'total': len(issueList), 'issueList': issueList}
-resultjson = json.dumps(result)
-print(resultjson)
+            page = page + 1
+
+        result = {'total': len(issueList), 'issueList': issueList}
+        resultjson = json.dumps(result)
+        print(resultjson)
+    except KeyError as e:
+        if retrytimes < tries - 1:
+            continue
+        else:
+            raise
+    break
