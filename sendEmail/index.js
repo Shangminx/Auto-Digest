b6c9a847e9dc8af1cea328464287c8595264c0b5
Shangminx <119831188+Shangminx@users.noreply.github.com>
2023-03-01 19:23:58 +0800

    Update index.js

--- sendEmail/index.js
+++ sendEmail/index.js
@@ -1,158 +1,102 @@
-const { includes } = require("resium");
-const core = require('@actions/core');
-const github = require('@actions/github');
-const nodemailer = require('nodemailer');
-
-const email_password = core.getInput('email_password');
-const email_username = core.getInput('sender_email');
-const report_data = JSON.parse(core.getInput('report_data'));
-const email_to = core.getInput('recipient_email').split(';');
-
-const keywords_lists = [
-    ["privacy", "theft", "steal", "leak"],
-    ["safety", "security", "concern"],
-    ["data", "password", "profile"], 
-    ["send", "user", "Microsoft","content"] 
-]
-  
-main(email_username, email_password, email_to, report_data)
-
-function main(email_username, email_password, email_to, report_data) {
-    try {
-        sendMail(email_username, email_password, email_to, report_data);
+<style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
     }
-    catch (err) {
-        core.setFailed(`Error ${err}`);
+    
+    th, td {
+      text-align: left;
+      padding: 8px;
     }
-}
+    
+    th {
+      background-color: #222;
+      color: #fff;
+    }
+    
+    /* tr:nth-child(even) {
+      background-color: #f2f2f2;
+    }
+    tr {
+    }
+    tr:hover {
+      background-color: #ddd;
+    } */
+</style>
+<html>
+    <body style="background-color:white">
+        <table align="center" border="0" cellpadding="0" cellspacing="0"
+            width="550" bgcolor="white" style="border:2px solid black">
+            <tbody>
+                <tr>
+                    <td align="center">
+                        <table align="center" border="0" cellpadding="0" 
+                            cellspacing="0" class="col-550" width="550">
+                            <tbody>
+                                <tr>
+                                    <td align="center" style="background-color: #188cd9;;
+                                            height: 50px;">
+                                        <a align="center" href="#" style="text-decoration: none;">
+                                            <p align="center" style="color:white;
+                                                    font-weight:bold; font-size: 18px">
+                                                GitHub Auto-Digest Bot
+                                            </p>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr style="height: 30px;">
+                    <td align="center" style="border: none;
+                            border-bottom: 2px solid #188cd9; 
+                            padding-right: 20px;padding-left:20px">
+    
+                        <p align="center" style="font-weight: bolder;font-size: 36px;
+                                letter-spacing: 0.025em;
+                                color:black" class="small">
+                                <script>
+                                    date = new Date().toLocaleDateString();
+                                    document.write(date);
+                                </script>
+                                Daily Isssue 
+                        </p>
+                    </td>
+                </tr>
 
-function sendMail(email_username, email_password, email_to, report_data) {
-
-    var listcontent = ""
-    for(let i = 0; i < report_data.total; i++)
-    {
-        listcontent = listcontent + `
+                <table>
+                    <thead>
+                    <tr align="center">
+                        <th>NO.</th>
+                        <th>ID</th>
+                        <th>TITLE</th>
+                        <th>TAG</th>
+                    </tr>
+                    </thead>
+                    <tbody>
                     <tr>
-                        <td>${report_data.issueList[i].issueName}</td>
-                        <td><a href=${report_data.issueList[i].issueLink}>${report_data.issueList[i].issueLink}</a></td>
-                        <td>${report_data.issueList[i].issueTags}</td>
-                        <td>${report_data.issueList[i].issueAssignee}</td>
+                        <td>1</td>
+                        <td>issueid</td>
+                        <td><a href="https://www.baidu.com">issuename</a></td>
+                        <td>question</td>
                     </tr>
-        `
-    }
-    const emailContent = `
-    <style>
-        table {
-        border-collapse: collapse;
-        width: 100%;
-        }
-        
-        th, td {
-        text-align: left;
-        padding: 8px;
-        }
-        
-        th {
-        background-color: #222;
-        color: #fff;
-        }
-        
-        /* tr:nth-child(even) {
-        background-color: #f2f2f2;
-        }
-        tr {
-        }
-        tr:hover {
-        background-color: #ddd;
-        } */
-    </style>
-    <html>
-        <body style="background-color:white">
-            <table align="center" border="0" cellpadding="0" cellspacing="0"
-                width="550" bgcolor="white" style="border:2px solid black">
-                <tbody>
                     <tr>
-                        <td align="center">
-                            <table align="center" border="0" cellpadding="0" 
-                                cellspacing="0" class="col-550" width="550">
-                                <tbody>
-                                    <tr>
-                                        <td align="center" style="background-color: #188cd9;;
-                                                height: 50px;">
-                                            <a align="center" href="#" style="text-decoration: none;">
-                                                <p style="color:white;
-                                                        font-weight:bold; font-size: 18px">
-                                                    GitHub Auto-Digest Bot
-                                                </p>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </td>
+                        <td>2</td>
+                        <td>issueid</td>
+                        <td><a href="https://www.baidu.com">issuename</a></td>
+                        <td>question</td>
                     </tr>
-                    <tr style="height: 30px;">
-                        <td align="center" style="border: none;
-                                border-bottom: 2px solid #188cd9; 
-                                padding-right: 20px;padding-left:20px">
-        
-                            <p align="center" style="font-weight: bolder;font-size: 36px;
-                                    letter-spacing: 0.025em;
-                                    color:black" class="small">
-                                    <script>
-                                        date = new Date().toLocaleDateString();
-                                        document.write(date);
-                                    </script>
-                                    Daily Isssue 
-                            </p>
-                        </td>
+                    <tr>
+                        <td>3</td>
+                        <td>issueid</td>
+                        <td><a href="https://www.baidu.com">issuename</a></td>
+                        <td>question</td>
                     </tr>
-
-                    <table>
-                        <thead>
-                        <tr align="center">
-                            <th>IssueName</th>
-                            <th>IssueLink</th>
-                            <th>TAG</th>
-                            <th>Assignee</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        ${listcontent}
-                        </tbody>
-                    </table>
-                </tbody>
-            </table>
-        </body>
-    </html>
-    `
-
-    let transporter = nodemailer.createTransport({
-        host: 'primary.exchange.microsoft.com',
-        port: 25,
-        tls: {
-            rejectUnauthorized: false
-        }
-    })
-
-    let mailOptions = {
-        from: email_username,
-        to: email_to,
-        subject: 'Daily report about issues',
-        html: emailContent,
-        priority: "high"
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            core.error(error);
-        } else {
-            core.info('Email sent: ' + info.response);
-        }
-    });
-
-}
-
-module.exports = {
-    main,
-}
+                    </tbody>
+                </table>
+            </tbody>
+        </table>
+    </body>
+</html>
+    
