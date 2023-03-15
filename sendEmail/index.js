416510dea81b1cf0303a9b8f58a6084d1856a0a6
Shangminx <119831188+Shangminx@users.noreply.github.com>
2023-03-15 16:43:52 +0800

    Update index.js

--- sendEmail/index.js
+++ sendEmail/index.js
@@ -152,23 +152,23 @@ function sendMail(email_username, email_password, email_to, report_data) {
         resolve(info);
     });
         
-    let info;
-    let error;
+    app.post('/sendmail', async (req, res) => {
+        let info;
+        let error;
 
-    for (let i = 0; i < 3; i++) {
-        try {
-          info = await promiseWrapper(mailOptions);
-          break;
-        } catch (e) {
-          error = e;
+        for (let i = 0; i < 3; i++) {
+            try {
+              info = await promiseWrapper(mailOptions);
+              break;
+            } catch (e) {
+              error = e;
+            }
         }
-    }
-    
-    if (error) {
-        core.error(error);
-    } else {
-        core.info('Email sent: ' + info.response);
-    }
+
+      info 
+        ? res.status(200).json({ message: "Mail Sent", response: info.response })
+        : res.status(500).json({ message: "Mail not send", error }));
+    });
 }
 
 module.exports = {
